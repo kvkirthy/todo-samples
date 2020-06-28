8c79a5f2985d39fa6aad68f0a2701548ddcad676
vencki <kvkirthy@gmail.com>
2020-06-28 03:02:44 -0400

    Date Range components added

--- memoization-demo/src/app/app.module.ts
+++ memoization-demo/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { todoReducer } from './ngrx/todo.reducer';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatNativeDateModule, MatRippleModule} from '@angular/material/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -15,6 +16,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import  { FormsModule } from '@angular/forms';
 
 import { CreateTodoComponent } from './create-todo/create-todo.component';
@@ -29,12 +31,15 @@ import { CreateTodoComponent } from './create-todo/create-todo.component';
     BrowserModule,    
     FormsModule,
     AppRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatCheckboxModule,
     MatSlideToggleModule,
     MatCardModule,
     MatToolbarModule,
     MatInputModule,
     MatButtonModule,
+    MatRippleModule,
     MatFormFieldModule,
     StoreModule.forRoot({ todos: todoReducer }),
     BrowserAnimationsModule
